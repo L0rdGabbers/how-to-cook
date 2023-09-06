a9d23fecc491d8dd48ee04cff770b42ff1c2e7e1
L0rdGabbers <>
2023-09-06 16:17:45 +0000

    Adds Recipe and Comment classes to models.py and adds corresponding classes in admin.py

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from statistics import mean
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Recipe(models.Model):
+    BEGINNER = "B"
+    INTERMEDIATE = "I"
+    EXPERT = "E"
+    DIFFICULTY_CHOICES = [
+        (BEGINNER, "Beginner"),
+        (INTERMEDIATE, "Intermediate"),
+        (EXPERT, "Expert")
+    ]
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="recipe_posts")
+    updated_on = models.DateTimeField(auto_now=True)
+    summary = models.TextField(blank=True)
+    featured_image = CloudinaryField('image', default='placeholder')
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    star_rating = models.ManyToManyField(
+        User, related_name="recipe_rating", blank=True)
+    time_to_prepare = models.PositiveIntegerField()
+    difficulty = models.CharField(
+        max_length=1, choices=DIFFICULTY_CHOICES, default=BEGINNER)
+    ingredients = models.TextField()
+    instructions = models.TextField()
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def average_star_rating(self):
+        average = mean(self.star_rating)
+        return round(average, 0)
+
+
+class Comment(models.Model):
+    recipe = models.ForeignKey(
+        Recipe, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField(default="")
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
