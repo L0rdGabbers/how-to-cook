1a470112106a5c418fcd21f3304dbf9530c4d25e
L0rdGabbers <>
2023-09-27 15:27:23 +0000

    Turns debug off

--- timetodine/settings.py
+++ timetodine/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-l0rdgabbers-how-to-cook-3to8mge8b6.us2.codeanyapp.com', 'time-to-dine-0c73767650ec.herokuapp.com']
